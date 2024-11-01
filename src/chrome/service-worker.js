ca979a9f76d9ee250eb587196cb90904e68e304c
whitmank <whitman.karter@gmail.com>
2024-11-01 14:48:16 -0400

    enabled side panel

---
+++ src/chrome/service-worker.js
@@ -0,0 +1,10 @@
+const GOOGLE_ORIGIN = 'https://www.google.com';
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+chrome.commands.onCommand.addListener((command) => {
+    console.log(`Command: ${command}`);
+  });
