43d9e64a694a37c6dffad7b3119ea9fa8f020f7a
sheasewall <sheasewall@gmail.com>
2024-11-06 16:08:02 -0500

    [Bugfix] Corrected save interval

--- src/chrome/sidePanel.js
+++ src/chrome/sidePanel.js
@@ -1,9 +1,5 @@
 console.log('sidePanel.js loaded at ' + new Date().toLocaleTimeString());
 
-
-const FORCE_MAX_ITER = 5;
-const SAVE_INTERVAL = 2000;
-
 import { Sigma } from 'sigma';
 import { UTILS, saveGraph, ensureGraph } from '/src/graphology/graphUtils.js';
 import ForceSupervisor from 'graphology-layout-force/worker';
@@ -20,19 +16,30 @@ const port = chrome.runtime.connect({ name: UTILS.CHROME_SIDE_PANEL_PORT_NAME })
 
 port.onMessage.addListener((message) => {
     const tabId = message.tabId;
+    console.log('sidePanel.js: Received message', message);
     if (message.action === UTILS.ADD_NODE_ACTION_CODE) {
         minimapGraph.addTabNode(tabId);
+        console.log('sidePanel.js: Node added', tabId);
     }
     if (message.action === UTILS.DROP_NODE_ACTION_CODE) {
         minimapGraph.removeTabNode(tabId);
+        console.log('sidePanel.js: Node removed', tabId);
     }
     if (message.action === UTILS.UPDATE_NODE_ACTION_CODE) {
         minimapGraph.updateTabNode(tabId, message.title, message.url);
+        console.log('sidePanel.js: Node updated', tabId, message.title, message.url);
+    }
+    if (message.action === UTILS.UPDATE_ACTIVE_TAB_ACTION_CODE) {
+        minimapGraph.activeTab = tabId;
+        console.log('sidePanel.js: Active tab updated to', tabId);
     }
     saveGraph(minimapGraph);
+    console.log('sidePanel.js: Graph saved after message processing');
 });
 
+console.log(UTILS.SAVE_INTERVAL);
+
 setInterval(() => {
-    console.log("Saving graph to session storage");
+    console.log('sidePanel.js: Interval triggered, saving graph');
     saveGraph(minimapGraph);
-}, UTILS.SAVE_INTERVAL);
+}, UTILS.SAVE_INTERVAL_MS);
